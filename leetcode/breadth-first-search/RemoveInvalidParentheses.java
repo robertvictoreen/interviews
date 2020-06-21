740d103effc7330c2047d446bda7aac07fd285cc
Robert Victoreen <robert.victoreen@yahoo.com>
2020-06-21 20:06:33 +0800

    Update RemoveInvalidParentheses.java

--- leetcode/breadth-first-search/RemoveInvalidParentheses.java
+++ leetcode/breadth-first-search/RemoveInvalidParentheses.java
@@ -7,44 +7,31 @@
 // "(a)())()" -> ["(a)()()", "(a())()"]
 // ")(" -> [""]
 
-public class RemoveInvalidParentheses {
+class Solution {
     public List<String> removeInvalidParentheses(String s) {
-        List<String> result = new ArrayList<>();
-        remove(s, result, 0, 0, new char[]{'(', ')'});
-        return result;
+        List<String> output = new ArrayList<>();
+        removeHelper(s, output, 0, 0, '(', ')');
+        return output;
     }
 
-    public void remove(String s, List<String> result, int last_i, int last_j,  char[] par) {
-        for (int stack = 0, i = last_i; i < s.length(); i++) {
-            if (s.charAt(i) == par[0]) {
-                stack++;
+    public void removeHelper(String s, List<String> output, int iStart, int jStart, char openParen, char closedParen) {
+        int numOpenParen = 0, numClosedParen = 0;
+        for (int i = iStart; i < s.length(); i++) {
+            if (s.charAt(i) == openParen) numOpenParen++;
+            if (s.charAt(i) == closedParen) numClosedParen++;
+            if (numClosedParen > numOpenParen) { // We have an extra closed paren we need to remove
+                for (int j = jStart; j <= i; j++) // Try removing one at each position, skipping duplicates
+                    if (s.charAt(j) == closedParen && (j == jStart || s.charAt(j - 1) != closedParen))
+                    // Recursion: iStart = i since we now have valid # closed parenthesis thru i. jStart = j prevents duplicates
+                        removeHelper(s.substring(0, j) + s.substring(j + 1, s.length()), output, i, j, openParen, closedParen);
+                return; // Stop here. The recursive calls handle the rest of the string.
             }
-
-            if (s.charAt(i) == par[1]) {
-                stack--;
-            }
-
-            if (stack >= 0) {
-                continue;
-            }
-            
-            for (int j = last_j; j <= i; j++) {
-                if (s.charAt(j) == par[1] && (j == last_j || s.charAt(j - 1) != par[1])) {
-                    remove(s.substring(0, j) + s.substring(j + 1, s.length()), result, i, j, par);
-                }
-            }
-
-            return;
         }
-        
+        // No invalid closed parenthesis detected. Now check opposite direction, or reverse back to original direction.
         String reversed = new StringBuilder(s).reverse().toString();
-        
-        if (par[0] == '(')  {
-            // finished left to right
-            remove(reversed, result, 0, 0, new char[]{')', '('});
-        } else {
-            // finished right to left
-            result.add(reversed);
-        }
+        if (openParen == '(')
+            removeHelper(reversed, output, 0, 0, ')','(');
+        else
+            output.add(reversed);
     }
 }
