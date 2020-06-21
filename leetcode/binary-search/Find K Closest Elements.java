70456dec4694daa2fdac1183b1289810456940e7
Robert Victoreen <robert.victoreen@yahoo.com>
2020-06-21 21:30:51 +0800

    Create Find K Closest Elements.java

---
+++ leetcode/binary-search/Find K Closest Elements.java
@@ -0,0 +1,34 @@
+/*
+Given a sorted array arr, two integers k and x, find the k closest elements to x in the array.
+The result should also be sorted in ascending order. If there is a tie, the smaller elements are always preferred.
+
+
+Example 1:
+
+Input: arr = [1,2,3,4,5], k = 4, x = 3
+Output: [1,2,3,4]
+Example 2:
+
+Input: arr = [1,2,3,4,5], k = 4, x = -1
+Output: [1,2,3,4]
+ 
+*/
+
+class Solution {
+    public List<Integer> findClosestElements(int[] arr, int k, int x) {
+        int lo = 0;
+		int hi = arr.length - 1;
+		while (hi - lo >= k) {
+			if (Math.abs(arr[lo] - x) > Math.abs(arr[hi] - x)) {
+				lo++;
+			} else {
+				hi--;
+			}
+		}
+		List<Integer> result = new ArrayList<>(k);
+		for (int i = lo; i <= hi; i++) {
+			result.add(arr[i]);
+		}
+		return result;
+    }
+}
